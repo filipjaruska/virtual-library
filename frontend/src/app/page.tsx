83f09dfd25db3d6c258f84b6f6891442689a5c8a
Filip Jaruška <70625490+filipjaruska@users.noreply.github.com>
2024-08-04 13:12:28 +0200

    add flattenAttributes function

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,14 @@
 import {Button} from "@/components/ui/button";
+import {flattenAttributes} from "@/lib/utils";
 
 async function run(path: string) {
     const baseUrl = "http://localhost:1337";
     try {
         const response = await fetch(baseUrl + path)
         const data = await response.json()
-        return data;
+        const lattenData = flattenAttributes(data)
+        console.dir(lattenData,  {depth: null})
+        return lattenData;
     } catch (error) {
         console.log(error)
     }
@@ -14,7 +17,7 @@ async function run(path: string) {
 export default async function Home() {
 
     const strapiData = await run("/api/home-page")
-    const {title, description} = strapiData.data.attributes;
+    const {title, description} = strapiData;
     return (
         <main>
             <h1>{title}</h1>
