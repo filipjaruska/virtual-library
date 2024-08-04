59ae435c440cac2dc01efc5e1cbe244df98daeee
Filip Jaruška <70625490+filipjaruska@users.noreply.github.com>
2024-08-04 14:47:08 +0200

    add hero-section

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,5 +1,60 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface LayoutHeroSection extends Schema.Component {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    displayName: 'Hero Section';
+  };
+  attributes: {
+    heading: Attribute.String;
+    subHeading: Attribute.Text;
+    image: Attribute.Media<'images'>;
+    link: Attribute.Component<'components.link'>;
+  };
+}
+
+export interface LayoutHeader extends Schema.Component {
+  collectionName: 'components_layout_headers';
+  info: {
+    displayName: 'Header';
+  };
+  attributes: {
+    logoText: Attribute.Component<'components.link'>;
+    ctaButton: Attribute.Component<'components.link'>;
+  };
+}
+
+export interface LayoutFooter extends Schema.Component {
+  collectionName: 'components_layout_footers';
+  info: {
+    displayName: 'Footer';
+  };
+  attributes: {
+    logoText: Attribute.Component<'components.link'>;
+    text: Attribute.Text;
+    socialLink: Attribute.Component<'components.link', true>;
+  };
+}
+
+export interface ComponentsLink extends Schema.Component {
+  collectionName: 'components_components_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    url: Attribute.String;
+    text: Attribute.String;
+    isExternal: Attribute.Boolean;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'layout.hero-section': LayoutHeroSection;
+      'layout.header': LayoutHeader;
+      'layout.footer': LayoutFooter;
+      'components.link': ComponentsLink;
+    }
+  }
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,29 @@
+import qs from 'qs'
 import {Button} from "@/components/ui/button";
 import {flattenAttributes} from "@/lib/utils";
+import {HeroSection} from "@/components/section/hero-section";
 
+const homePageQuery = qs.stringify({
+    populate: {
+        blocks: {
+            populate: {
+                image: {
+                    fields: ["url", "alternativeText"],
+                },
+                link: {
+                    populate: true,
+                }
+            }
+        }
+    }
+});
 async function run(path: string) {
     const baseUrl = "http://localhost:1337";
+    
+    const url = new URL(path, baseUrl);
+    url.search = homePageQuery;
     try {
-        const response = await fetch(baseUrl + path)
+        const response = await fetch(url.href, {cache: "no-store"});
         const data = await response.json()
         const lattenData = flattenAttributes(data)
         console.dir(lattenData,  {depth: null})
@@ -17,12 +36,10 @@ async function run(path: string) {
 export default async function Home() {
 
     const strapiData = await run("/api/home-page")
-    const {title, description} = strapiData;
+    const {title, description, blocks} = strapiData;
     return (
         <main>
-            <h1>{title}</h1>
-            <p>{description}</p>
-            <Button>Button</Button>
+            <HeroSection data={blocks[0]}/>
         </main>
     );
 }
