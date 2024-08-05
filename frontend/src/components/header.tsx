ec74d99c317a5fbbe633de0324055555724ec1a1
Filip Jaruška <70625490+filipjaruska@users.noreply.github.com>
2024-08-05 18:25:05 +0200

    add user icon dropdown menu

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -2,6 +2,16 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import {getUserMeLoader} from "@/lib/services/get-user-me-loader";
 import {LogoutButton} from "@/components/ui/logout-button";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 
 interface HeaderProps {
     data: {
@@ -24,8 +34,25 @@ export async function Header({ data }: Readonly<HeaderProps>) {
     return (
         <div className="flex items-center justify-between px-4 py-3 bg-white shadow-md dark:bg-gray-800">
             <div>{logoText.text}</div>
-            <div className="flex items-center gap-4">
-                {!user.ok ? <Link href={ctaButton.url}><Button>{ctaButton.text}</Button></Link> : <LogoutButton/>}
+            <div style={{ userSelect: "none" }} className="flex items-center gap-4">
+                {!user.ok ? <Link href={ctaButton.url}><Button>{ctaButton.text}</Button></Link> :
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Avatar className="cursor-pointer hover:scale-105">
+                                <AvatarImage src="https://github.com/shadcn.png" />
+                                <AvatarFallback>{user.data.username.slice(0,2)}</AvatarFallback>
+                            </Avatar>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuLabel style={{ userSelect: "none" }}>{user.data.username}</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <Link href={"/dashboard"}><DropdownMenuItem className="cursor-pointer" >Profile</DropdownMenuItem></Link>
+                            <DropdownMenuItem className="cursor-pointer">Settings</DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuLabel><LogoutButton/></DropdownMenuLabel>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                }
             </div>
         </div>
     );
--- frontend/src/components/ui/logout-button.tsx
+++ frontend/src/components/ui/logout-button.tsx
@@ -4,8 +4,9 @@ import { LogOut } from "lucide-react";
 export function LogoutButton() {
     return (
         <form action={logoutAction}>
-            <button type="submit">
-                <LogOut className="w-6 h-6 hover:text-primary" />
+            <button type="submit" className="flex flex-row items-center justify-center font-medium">
+                <div style={{ userSelect: "none" }} className="pr-1 text-l">Logout</div>
+                <LogOut className="w-4 h-4 hover:text-primary" />
             </button>
         </form>
     );
