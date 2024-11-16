510b04a893c072db3a5d2dcd5282cc4ae4c0519b
Filip Jaruška <70625490+filipjaruska@users.noreply.github.com>
2024-11-16 15:42:09 +0100

    update hostname

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "http",
-                hostname: "localhost",
-                port: "1337",
-                pathname: "/uploads/**/*",
-            },
-            {
-                protocol: "https",
-                hostname: "tasteful-respect-ed15c7ec66.media.strapiapp.com",
-            }
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**/*",
+      },
+      {
+        protocol: "https",
+        hostname: "mighty-citadel-92312-bd2c74c8f386.herokuapp.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
