e7e175098663b4ae97b01aa86580c99bcf20af1b
Filip Jaruška <70625490+filipjaruska@users.noreply.github.com>
2024-11-19 11:17:57 +0100

    add todos and console logs

--- frontend/src/lib/services/get-token.ts
+++ frontend/src/lib/services/get-token.ts
@@ -1,6 +1,8 @@
+"use server";
 import { cookies } from "next/headers";
 
 export async function getAuthToken() {
   const authToken = (await cookies()).get("jwt")?.value;
+  console.log("Auth Token:", authToken); //TODO remove
   return authToken;
 }
--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -2,16 +2,19 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getUserMeLoader } from "@/lib/services/get-user-me-loader";
 
+// TODO middleware the issue?
+
 export async function middleware(request: NextRequest) {
-    const user = await getUserMeLoader();
-    const currentPath = request.nextUrl.pathname;
+  const user = await getUserMeLoader();
+  console.log("User Middleware:", user); //TODO remove
+  const currentPath = request.nextUrl.pathname;
 
-    if (currentPath.startsWith("/dashboard") && user.ok === false) {
-        return NextResponse.redirect(new URL("/signin", request.url));
-    }
-    if (currentPath.startsWith("/books") && user.ok === false) {
-        return NextResponse.redirect(new URL("/signup", request.url));
-    }
+  if (currentPath.startsWith("/dashboard") && user.ok === false) {
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+  if (currentPath.startsWith("/books") && user.ok === false) {
+    return NextResponse.redirect(new URL("/signup", request.url));
+  }
 
-    return NextResponse.next();
+  return NextResponse.next();
 }
