0dd2ce9015df40af5dbc265a955fa5175ba028ab
Filip Jaruška <70625490+filipjaruska@users.noreply.github.com>
2024-11-19 17:01:42 +0100

    fix: logout not working in production

--- frontend/src/components/ui/logout-button.tsx
+++ frontend/src/components/ui/logout-button.tsx
@@ -1,13 +1,19 @@
-import { logoutAction } from "@/lib/actions/auth-actions"
+import { logoutAction } from "@/lib/actions/auth-actions";
 import { LogOut } from "lucide-react";
 
 export function LogoutButton() {
+    const handleLogout = async (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        await logoutAction();
+    };
+
     return (
-        <form action={logoutAction}>
-            <button type="submit" className="flex flex-row items-center justify-center font-medium">
-                <div style={{ userSelect: "none" }} className="pr-1 text-l">Logout</div>
-                <LogOut className="w-4 h-4 hover:text-primary" />
-            </button>
-        </form>
+        <button
+            onClick={handleLogout}
+            className="flex flex-row items-center justify-center font-medium"
+        >
+            <div style={{ userSelect: "none" }} className="pr-1 text-l">Logout</div>
+            <LogOut className="w-4 h-4 hover:text-primary" />
+        </button>
     );
 }
